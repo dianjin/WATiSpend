f3d2f0ee92344d8dcc984c950b125951ad90cb4b
Dian Jin <gryffindorstudent@gmail.com>
2015-01-12 22:38:37 -0500

    Add google analytics code to track pages

--- includes/login/submit.php
+++ includes/login/submit.php
@@ -285,6 +285,17 @@ Muse.Utils.transformMarkupToFixBrowserProblems();/* body */
     <div style="float: right;" id="contact" class="link">Contact</div>
     <div style="margin-left: auto; margin-right: auto;" id="help" class="link">Help</div>
    </div>
+
+   <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58514515-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
    
    <!-- FOOTER ENDS HERE -->
 </body>
